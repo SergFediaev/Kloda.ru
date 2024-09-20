811765883138458c13757c7d20e9fb2320d5a01e
SergFediaev <sergfediaev@gmail.com>
2024-09-20 15:28:27 +0300

    refactor: create card form styles

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -10,7 +10,7 @@ export default function Create() {
   return (
     <div className='w-full flex flex-grow justify-center items-center p-8'>
       <div className='w-full max-w-xl flex flex-col gap-8 rounded-3xl p-8 shadow-lg bg-neutral-200 dark:bg-neutral-800'>
-        <h2 className='text-2xl'>Create card</h2>
+        <h2 className='text-2xl self-center'>Create card</h2>
         <CardForm />
         <span className='self-center'>
           ← <Link href='/'>Back to cards</Link>
