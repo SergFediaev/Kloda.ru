a593882c0cf349c78d983977a5e5797973514130
SergFediaev <sergfediaev@gmail.com>
2024-10-23 19:52:37 +0300

    fix: cards mode button only on cards page.

--- src/components/header/menu/nav/cardsModeButton.tsx
+++ src/components/header/menu/nav/cardsModeButton.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@/components/button'
 import { useCardsMode } from '@/hooks/useCardsMode'
 import { Dumbbell, GraduationCap } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 
 export const CardsModeButton = () => {
   const { isStudyMode, toggleStudyMode } = useCardsMode()
+
+  if (usePathname() !== '/') {
+    return null
+  }
+
   const modeIcon = isStudyMode ? <Dumbbell /> : <GraduationCap />
   const modeTitle = isStudyMode ? 'Enable practice mode' : 'Enable study mode'
 
