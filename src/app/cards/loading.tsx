32d02f6dd7fc683480341d0cfac3f4f1c55d9840
NatteNata <153759340+NatteNata@users.noreply.github.com>
2025-01-09 08:00:19 +0300

    Loading and Error for Cards (#6)
    refactor: loading and error pages for Cards

---
+++ src/app/cards/loading.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@/components/containers/container'
+import { Loader } from '@/components/loader'
+
+export default function Loading() {
+  return (
+    <Container>
+      <Loader />
+    </Container>
+  )
+}
--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -19,7 +19,6 @@ type Props = {
   }
 }
 
-// ToDo: README.md
 export default function CardsPage({ searchParams }: Props) {
   return (
     <Container>
