2b042c4206c3298e6e6523ec089723fba2fb755c
NatteNata <natte@yandex.ru>
2025-01-09 10:51:28 +0300

    refactor: study-mode link

--- src/components/header/menu/nav/cardsModeButton.tsx
+++ src/components/header/menu/nav/cardsModeButton.tsx
@@ -1,20 +1,24 @@
 import { Button } from '@/components/buttons/button'
-import { usePaths } from '@/hooks/usePaths'
 import { cardsModeStore } from '@/stores/cardsModeStore'
 import { Dumbbell, GraduationCap } from 'lucide-react'
+import { usePathname } from 'next/navigation'
 
 export const CardsModeButton = () => {
   const { isStudyMode, toggleStudyMode } = cardsModeStore()
 
-  if (usePaths().isNotCardablePath) {
-    return null
-  }
+  const pathName = usePathname()
+  const enabled = pathName === '/cards'
 
   const modeIcon = isStudyMode ? <Dumbbell /> : <GraduationCap />
   const modeTitle = isStudyMode ? 'Enable practice mode' : 'Enable study mode'
 
   return (
-    <Button variant='text' title={modeTitle} onClick={toggleStudyMode}>
+    <Button
+      disabled={!enabled}
+      variant='text'
+      title={modeTitle}
+      onClick={toggleStudyMode}
+    >
       {modeIcon}
     </Button>
   )
