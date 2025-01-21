b3c6b5e8ee4c66c4b4a4758a34950336209d0ceb
NatteNata <natte@yandex.ru>
2025-01-22 02:32:09 +0300

    feat: tech carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function HomePage() {
       <CoreFeaturesSection />
       <StatementSection />
       <FeaturesSection />
-      {/*  <TechSection />*/}
+      <TechSection />
     </div>
   )
 }
---
+++ src/components/landing/sections/teckSection.tsx
@@ -0,0 +1,14 @@
+import TechCarousel from '@/components/carousel/techstackCarousel/techCarousel'
+import { Section } from '@/components/landing/section'
+import type { EmblaOptionsType } from 'embla-carousel'
+import React from 'react'
+
+const OPTIONS: EmblaOptionsType = { loop: true, align: 'start' }
+
+export function TechSection() {
+  return (
+    <Section className='bg-surface text-surface-dark dark:bg-surface-dark dark:text-surface'>
+      <TechCarousel options={OPTIONS} />
+    </Section>
+  )
+}
