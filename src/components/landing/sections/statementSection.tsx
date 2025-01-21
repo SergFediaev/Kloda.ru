175c28c232fb092b68ea5a050027db67cdf50914
NatteNata <natte@yandex.ru>
2025-01-22 02:06:49 +0300

    fix: small fixes

--- src/components/landing/sections/statementSection.tsx
+++ src/components/landing/sections/statementSection.tsx
@@ -4,7 +4,7 @@ import { Summary } from '@/components/landing/summary'
 
 export const StatementSection = () => {
   return (
-    <Section className='dark:selection:bg-white'>
+    <Section className='pb-0 dark:selection:bg-white'>
       <Container className='gap-5'>
         <h2 className='text-center font-extrabold text-4xl text-accent-variant dark:text-[hsl(24,100%,50%)]'>
           Be proud of the better you for studying smarter ...and faster.
