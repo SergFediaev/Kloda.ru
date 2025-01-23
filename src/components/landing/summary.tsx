26a513632ac9da754564ab50bb0b5672ffeeecfd
NatteNata <153759340+NatteNata@users.noreply.github.com>
2025-01-24 01:09:01 +0300

    fix: statement section (#13)
    fix: statement section

---
+++ src/components/landing/summary.tsx
@@ -0,0 +1,12 @@
+import { Container } from '@/components/containers/container'
+
+export const Summary = () => {
+  return (
+    <Container>
+      <p className='text-center font-semibold text-accent-variant text-xl dark:text-accent-neon'>
+        Reinforce your knowledge and prepare for any interview or exam with
+        confidence and Kloda.
+      </p>
+    </Container>
+  )
+}
