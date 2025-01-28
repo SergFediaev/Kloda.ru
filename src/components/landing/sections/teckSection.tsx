a620996b03b999645c0a18fd313be1a9eae12609
NatteNata <natte@yandex.ru>
2025-01-28 08:53:56 +0300

    fix: tooltip WIP

--- src/components/landing/sections/teckSection.tsx
+++ src/components/landing/sections/teckSection.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import { TechCarousel } from '@/components/carousel/techstackCarousel/techCarousel'
 import { Section } from '@/components/landing/section'
+import { Tooltippy } from '@/components/tooltip/tooltippy'
 import type { EmblaOptionsType } from 'embla-carousel'
 
 const OPTIONS: EmblaOptionsType = {
@@ -7,10 +10,17 @@ const OPTIONS: EmblaOptionsType = {
   align: 'start',
 }
 
-export function TechSection() {
+export const TechSection = () => {
   return (
     <Section className='bg-surface p-5 text-surface-dark dark:bg-surface-dark dark:text-surface'>
-      <TechCarousel options={OPTIONS} />
+      <Tooltippy
+        anchorId='tech-anchor'
+        content='Our trusted tech stack for this App!'
+        float={true}
+        offset={30}
+      >
+        <TechCarousel options={OPTIONS} />
+      </Tooltippy>
     </Section>
   )
 }
