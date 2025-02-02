7fcf1942823d20e648cb760e3717de404a099cf2
NatteNata <natte@yandex.ru>
2025-02-02 07:04:39 +0300

    refactor: default params

--- src/app/cards/page.tsx
+++ src/app/cards/page.tsx
@@ -1,5 +1,9 @@
 import { Cards } from '@/components/cards/cards'
 import { Container } from '@/components/containers/container'
+import {
+  CARDS_DEFAULT_PARAMS,
+  type CardsSearchParams,
+} from '@/components/displayOptions/pageControls'
 import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
@@ -7,37 +11,13 @@ export const metadata: Metadata = {
 }
 
 type Props = {
-  searchParams?: {
-    search?: string
-    page?: string
-    limit?: string
-    order?: string
-    sort?: string
-    categories?: string | string[]
-    userId?: string
-    action?: string
-  }
+  searchParams?: CardsSearchParams
 }
 
 export default function CardsPage({ searchParams }: Props) {
   return (
     <Container>
-      <Cards
-        search={searchParams?.search ?? ''}
-        page={Number(searchParams?.page) || 1}
-        limit={Number(searchParams?.limit) || 10}
-        order={searchParams?.order ?? 'desc'}
-        sort={searchParams?.sort ?? 'createdAt'}
-        categories={
-          searchParams?.categories
-            ? Array.isArray(searchParams.categories)
-              ? searchParams.categories
-              : [searchParams.categories]
-            : []
-        }
-        userId={Number(searchParams?.userId) || undefined}
-        action={searchParams?.action}
-      />
+      <Cards {...CARDS_DEFAULT_PARAMS} {...searchParams} />
     </Container>
   )
 }
