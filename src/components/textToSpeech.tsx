73128cb12a0f6d74ad0cdc7521a9ba61dda56820
NatteNata <natte@yandex.ru>
2025-02-02 07:03:19 +0300

    refactor: syntax

--- src/components/textToSpeech.tsx
+++ src/components/textToSpeech.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import type { CardModel } from '@/api/cards/cards.types'
 import { Button } from '@/components/buttons/button'
 import { ShareButton } from '@/components/buttons/shareButton'
@@ -32,24 +34,6 @@ import { useTheme } from 'next-themes'
 import Image from 'next/image'
 import { type ChangeEvent, type ReactElement, useEffect, useState } from 'react'
 
-type Props = {
-  cards: CardModel[]
-  cardToSpeech?: CardModel
-  setCardToSpeech: (card?: CardModel) => void
-  setIsCardPlaying?: (isPlaying: boolean) => void
-  playlistName?: string
-}
-
-export type PlayMode = 'once' | 'repeat' | 'playlist' | 'shuffle'
-
-let speechSynth: Nullable<SpeechSynthesis> = null
-let utterance: Nullable<SpeechSynthesisUtterance> = null
-
-if (typeof window !== 'undefined' && window.speechSynthesis) {
-  speechSynth = speechSynthesis
-  utterance = new SpeechSynthesisUtterance()
-}
-
 const DEFAULT_VOICE = 0
 const DEFAULT_LANG = 'en-US'
 const LANG_MAP: Record<string, string> = {
@@ -60,10 +44,29 @@ const LANG_MAP: Record<string, string> = {
   deu: 'de-DE',
 } as const
 
+export type PlayMode = 'once' | 'repeat' | 'playlist' | 'shuffle'
+
+let speechSynth: Nullable<SpeechSynthesis> = null
+
+let utterance: Nullable<SpeechSynthesisUtterance> = null
+
+if (typeof window !== 'undefined' && window.speechSynthesis) {
+  speechSynth = speechSynthesis
+  utterance = new SpeechSynthesisUtterance()
+}
+
 const getCardText = ({ title, content }: CardModel) => `${title}\n${content}`
 
-const getCardIndex = (cards: CardModel[], cardId: number) =>
+const getCardIndex = (cards: CardModel[], cardId: string) =>
   cards.findIndex(({ id }) => id === cardId)
+
+type Props = {
+  cards: CardModel[]
+  cardToSpeech?: CardModel
+  setCardToSpeech: (card?: CardModel) => void
+  setIsCardPlaying?: (isPlaying: boolean) => void
+  playlistName?: string
+}
 
 export const TextToSpeech = ({
   cards,
