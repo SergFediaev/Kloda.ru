b91fcabb400b0d57d24071fcf6026709f06d9974
NatteNata <natte@yandex.ru>
2025-03-22 18:24:58 +0300

    fix: revert to old flow on focus view

--- src/components/cards/cardActionBar/focusCard.tsx
+++ src/components/cards/cardActionBar/focusCard.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/buttons/button'
+import { usePaths } from '@/hooks/usePaths'
 import { useTransitionRouter } from 'next-view-transitions'
 
 type Props = {
@@ -8,11 +9,12 @@ type Props = {
 
 export const FocusCard = ({ cardId, isOpen }: Props) => {
   const router = useTransitionRouter()
+  const { cardsPath } = usePaths()
 
   const cardDetailsText = isOpen ? 'Close focus view' : 'Open focus view'
 
   const handleFocus = () => {
-    !isOpen ? router.push(`/card/${cardId}`) : router.back()
+    !isOpen ? router.push(`/card/${cardId}`) : router.push(cardsPath)
   }
 
   return (
