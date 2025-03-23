088b2595f4d6e2cb8a176f9d28a41b540dc9be5a
NatteNata <153759340+NatteNata@users.noreply.github.com>
2025-03-23 13:53:56 +0300

    fix: eye icon for password fields in form (#32)
    fix: eye icon for password fields in form

--- src/components/forms/input.tsx
+++ src/components/forms/input.tsx
@@ -1,10 +1,12 @@
 import { Subfield } from '@/components/forms/subfield'
 import { useGenerateId } from '@/hooks/useGenerateId'
 import { cn } from '@/utils/mergeClasses'
+import { Eye, EyeOff } from 'lucide-react'
 import {
   type ComponentPropsWithoutRef,
   type ReactNode,
   forwardRef,
+  useState,
 } from 'react'
 
 export type InputProps = {
@@ -24,6 +26,7 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       isHorizontal,
       hasBorder,
       id,
+      type,
       name,
       required,
       maxLength,
@@ -33,6 +36,15 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     ref,
   ) => {
     const inputId = useGenerateId(id, name)
+    const [showPassword, setShowPassword] = useState(false)
+
+    const toggleShowPassword = () => {
+      setShowPassword(!showPassword)
+    }
+
+    const isPassword = type === 'password'
+    const passwordIcon = showPassword ? <Eye /> : <EyeOff />
+    const currentType = !isPassword ? type : showPassword ? 'text' : 'password'
 
     return (
       <div
@@ -51,19 +63,32 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
         >
           {label}
         </label>
-        <input
-          ref={ref}
-          id={inputId}
-          name={name}
-          required={required}
-          maxLength={maxLength}
-          className={cn(
-            'truncate rounded-xl px-4 py-2 shadow-inner',
-            hasBorder && 'border-2 border-accent dark:border-accent-dark',
-            restProps.type === 'number' && 'text-right',
+        <div className='flex items-center'>
+          <input
+            ref={ref}
+            id={inputId}
+            name={name}
+            required={required}
+            maxLength={maxLength}
+            type={currentType}
+            className={cn(
+              'w-full flex-1 pr-12',
+              'truncate rounded-xl py-2 pl-4 shadow-inner',
+              hasBorder && 'border-2 border-accent dark:border-accent-dark',
+              type === 'number' && 'text-right',
+            )}
+            {...restProps}
+          />
+          {isPassword && (
+            <button
+              type='button'
+              onClick={toggleShowPassword}
+              className='-ml-12 px-3 outline-none'
+            >
+              {passwordIcon}
+            </button>
           )}
-          {...restProps}
-        />
+        </div>
         <Subfield
           error={error}
           characterCount={characterCount}
